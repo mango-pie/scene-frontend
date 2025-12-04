41ad7182ee6c340cda57e1c2e6ad5d5a3d30f77b
mango-pie <19936458177@163.com>
2025-12-04 18:36:29 +0800

    sce

--- scene-backend/src/main/java/com/scenebackend/service/impl/ImageUploadServiceImpl.java
+++ scene-backend/src/main/java/com/scenebackend/service/impl/ImageUploadServiceImpl.java
@@ -27,7 +27,7 @@ public class ImageUploadServiceImpl implements ImageUploadService {
     private UploadConfig uploadConfig;
 
     // 最大文件大小(MB)
-    @Value("${upload.max-size:10")
+    @Value("5")
     private long maxFileSize;
 
     // 应用访问基础URL
